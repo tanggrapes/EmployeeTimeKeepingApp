3f8effa2d56c3f2ee5bac8314bfe58b30ace4966
Mark Jayson Toledo <markjaysonprietotoledo@gmail.com>
2020-11-07 17:54:24 +0800

    FEAT: Migrations Folder

---
+++ EmployeeTimeKeepingApp/Migrations/20201107080807_Initial.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeTimeKeepingApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Gender = table.Column<string>(nullable: false),
+                    DateHired = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionTypes",
+                columns: table => new
+                {
+                    TransactionTypeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TransactionTypName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionTypes", x => x.TransactionTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeKeepingTransactions",
+                columns: table => new
+                {
+                    TimeKeepingTransactionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    TransactionTypeId = table.Column<int>(nullable: false),
+                    TransactionDateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeKeepingTransactions", x => x.TimeKeepingTransactionId);
+                    table.ForeignKey(
+                        name: "FK_TimeKeepingTransactions_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TimeKeepingTransactions_TransactionTypes_TransactionTypeId",
+                        column: x => x.TransactionTypeId,
+                        principalTable: "TransactionTypes",
+                        principalColumn: "TransactionTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TransactionTypes",
+                columns: new[] { "TransactionTypeId", "TransactionTypName" },
+                values: new object[] { 1, "IN" });
+
+            migrationBuilder.InsertData(
+                table: "TransactionTypes",
+                columns: new[] { "TransactionTypeId", "TransactionTypName" },
+                values: new object[] { 2, "OUT" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeKeepingTransactions_EmployeeId",
+                table: "TimeKeepingTransactions",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeKeepingTransactions_TransactionTypeId",
+                table: "TimeKeepingTransactions",
+                column: "TransactionTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeKeepingTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "TransactionTypes");
+        }
+    }
+}
