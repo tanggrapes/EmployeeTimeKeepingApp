4599a9dfaa17bfd338aa6df79f041cbb0ed01b87
Mark Jayson Toledo <markjaysonprietotoledo@gmail.com>
2020-11-07 17:53:49 +0800

    FEAT: DbContext

---
+++ EmployeeTimeKeepingApp/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeTimeKeepingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EmployeeTimeKeepingApp.DataAccess
+{
+    public class ApplicationDbContext: DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<TransactionType> TransactionTypes { get; set; }
+        public DbSet<TimeKeepingTransaction> TimeKeepingTransactions { get; set; }
+
+        public Task<int> SaveChangesAsync() => base.SaveChangesAsync();
+
+        public Task<IDbContextTransaction> Transaction() => base.Database.BeginTransactionAsync();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.TimeKeepingTransactions)
+                .WithOne(t => t.Employee)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TransactionType>()
+                .HasMany(t => t.TimeKeepingTransactions)
+                .WithOne(t => t.TransactionType)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TransactionType>()
+                .HasData(
+                new TransactionType() { TransactionTypName = "IN", TransactionTypeId = 1 },
+                new TransactionType() { TransactionTypName = "OUT" , TransactionTypeId = 2}
+                );
+
+
+        }
+
+    }
+}
---
+++ EmployeeTimeKeepingApp/DataAccess/IApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeTimeKeepingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTimeKeepingApp.DataAccess
+{
+    public interface IApplicationDbContext
+    {
+        Task<int> SaveChangesAsync();
+
+        Task<IDbContextTransaction> Transaction();
+        DbSet<Employee> Employees { get; set; }
+        DbSet<TransactionType> TransactionTypes { get; set; }
+        DbSet<TimeKeepingTransaction> TimeKeepingTransactions { get; set; }
+    }
+}
