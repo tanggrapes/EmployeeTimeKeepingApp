16a354a91d6018af34a6e672af867c80f8d4c659
Mark Jayson Toledo <markjaysonprietotoledo@gmail.com>
2020-11-07 17:56:57 +0800

    FEAT: TransactionTypeService

---
+++ EmployeeTimeKeepingApp/Services/ITransactionTypeService.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeTimeKeepingApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTimeKeepingApp.Services
+{
+    public interface ITransactionTypeService
+    {
+        Task<List<TransactionType>> GetTransactionTypes();
+    }
+}
---
+++ EmployeeTimeKeepingApp/Services/TransactionTypeService.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeTimeKeepingApp.DataAccess;
+using EmployeeTimeKeepingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTimeKeepingApp.Services
+{
+    public class TransactionTypeService : ITransactionTypeService
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+        public TransactionTypeService(IApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+        public Task<List<TransactionType>> GetTransactionTypes()
+        {
+            return _applicationDbContext.TransactionTypes.ToListAsync();
+        }
+    }
+}
