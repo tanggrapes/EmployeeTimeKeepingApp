26b4c70f450acfca534e066984a57a843c1ac547
Mark Jayson Toledo <markjaysonprietotoledo@gmail.com>
2020-11-07 17:56:44 +0800

    FEAT: TimeKeepingTransactionService

---
+++ EmployeeTimeKeepingApp/Services/ITimeKeepingTransactionService.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeTimeKeepingApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTimeKeepingApp.Services
+{
+    public interface ITimeKeepingTransactionService
+    {
+
+        Task<List<TimeKeepingTransaction>> GetTimeKeepingTransactions(int pageSize, int page);
+        Task<int> Create(TimeKeepingTransaction timeKeepingTransaction);
+        Task<int> Count();
+        Task<int> CountByEmployee(int employeeId);
+        Task<List<TimeKeepingTransaction>> GetTimeKeepingTransactionsByEmployee(int pageSize, int page, int employeeId);
+    }
+}
---
+++ EmployeeTimeKeepingApp/Services/TimeKeepingTransactionService.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeTimeKeepingApp.DataAccess;
+using EmployeeTimeKeepingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTimeKeepingApp.Services
+{
+    public class TimeKeepingTransactionService : ITimeKeepingTransactionService
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+        public TimeKeepingTransactionService(IApplicationDbContext applicationDbContext)
+        {
+            this._applicationDbContext = applicationDbContext;
+        }
+
+        public Task<int> Count()
+        {
+            return _applicationDbContext.TimeKeepingTransactions.CountAsync();
+        }
+
+        public Task<int> CountByEmployee(int employeeId)
+        {
+            return _applicationDbContext.TimeKeepingTransactions.Where(t=> t.EmployeeId == employeeId).CountAsync();
+        }
+
+        public Task<int> Create(TimeKeepingTransaction timeKeepingTransaction)
+        {
+            _applicationDbContext.TimeKeepingTransactions.AddAsync(timeKeepingTransaction);
+            return _applicationDbContext.SaveChangesAsync();
+        }
+
+        public Task<List<TimeKeepingTransaction>> GetTimeKeepingTransactions(int pageSize, int page)
+        {
+            return _applicationDbContext.TimeKeepingTransactions.Include(e=> e.Employee).Include(e=>e.TransactionType).Skip(pageSize * page).Take(pageSize).ToListAsync();
+
+        }
+
+        public Task<List<TimeKeepingTransaction>> GetTimeKeepingTransactionsByEmployee(int pageSize, int page, int employeeId)
+        {
+            return _applicationDbContext.TimeKeepingTransactions.Include(e => e.Employee).Include(e => e.TransactionType).Where(t=> t.EmployeeId == employeeId).Skip(pageSize * page).Take(pageSize).ToListAsync();
+
+        }
+    }
+}
